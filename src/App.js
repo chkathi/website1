a2350a058a18ff0053806af10d3ceafaad08c734
Chandu Kathi <chandukathi@csus.edu>
2024-02-04 01:13:50 -0800

    Fixed weird Projects Error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./NavBar.js";
 import { Home } from "./pages/Home.js";
 import { Resume } from "./pages/Resume.js";
+import { Projects } from "./pages/Projects.js";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="*" element={<h1>404: Error Page</h1>} />
         </Routes>
       </Router>
---
+++ src/pages/Projects.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const Projects = () => {
+  return (
+    <div>
+      <h1>P1</h1>
+    </div>
+  );
+};
